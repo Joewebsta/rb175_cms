7ce95b733db1ed4ba0dc09bb427c07dc8881d7f0
Joe Webster <joseph.scott.webster@gmail.com>
2021-11-12 07:46:56 -0500

    Write tests for / and /:filename routes

--- cms.rb
+++ cms.rb
@@ -6,13 +6,25 @@ require 'tilt/erubis'
 
 root = File.expand_path(__dir__)
 
+configure do
+  enable :sessions
+  set :session_secret, 'super super secret'
+end
+
 get '/' do
   @files = Dir.glob(root + '/data/*').map { |path| File.basename(path) }
+  headers['Content-Type'] = 'text/html;charset=utf-8'
   erb :index
 end
 
 get '/:filename' do
   file_path = root + '/data/' + params[:filename]
-  headers['Content-Type'] = 'text/plain'
-  File.read(file_path)
+
+  if File.file?(file_path)
+    headers['Content-Type'] = 'text/plain'
+    File.read(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -24,11 +24,23 @@ class CmsTest < Minitest::Test
     assert_includes last_response.body, 'history.txt'
   end
 
-  def test_files
+  def test_viewing_text_document
     get '/about.txt'
 
     assert_equal 200, last_response.status
     assert_equal 'text/plain', last_response['Content-Type']
     assert_includes last_response.body, 'Ruby 0.95 released'
+  end
+
+  def test_document_not_found
+    get '/joe.txt'
+    assert_equal 302, last_response.status
+
+    get '/'
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'joe.txt does not exist.'
+
+    get '/'
+    refute_includes last_response.body, 'joe.txt does not exist.'
   end
 end
